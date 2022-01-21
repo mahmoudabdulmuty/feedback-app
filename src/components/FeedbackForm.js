fc712de1529fb949df731fcfd3710e02d906e44f
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-21 11:26:02 +0200

    Rating Select Component

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
+import RatingSelect from './RatingSelect';
 import Button from './shared/Button';
 import Card from './shared/Card';
 export default function FeedbackForm() {
 	const [review, setReview] = useState('');
+	const [rating, setRating] = useState(10);
 	const [isDisabled, setIsDisabled] = useState(true);
 	const [message, setMessage] = useState('');
 
@@ -24,6 +26,7 @@ export default function FeedbackForm() {
 		<Card>
 			<form>
 				<h2>How would you rate your service with us ?</h2>
+				<RatingSelect select={(rating) => setRating(rating)} />
 				<div className="input-group">
 					<input
 						onChange={handleChange}
